4a3be6a43b595dce4d19f6f3174e64282c8d9362
theodavid <theodavid@ut.ee>
2026-03-04 19:14:43 +0200

    HelloService->CheckFraud

--- utils/pb/fraud_detection/fraud_detection_pb2.pyi
+++ utils/pb/fraud_detection/fraud_detection_pb2.pyi
@@ -4,14 +4,16 @@ from typing import ClassVar as _ClassVar, Optional as _Optional
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
-class HelloRequest(_message.Message):
-    __slots__ = ("name",)
-    NAME_FIELD_NUMBER: _ClassVar[int]
-    name: str
-    def __init__(self, name: _Optional[str] = ...) -> None: ...
+class FraudRequest(_message.Message):
+    __slots__ = ("card_number", "order_amount")
+    CARD_NUMBER_FIELD_NUMBER: _ClassVar[int]
+    ORDER_AMOUNT_FIELD_NUMBER: _ClassVar[int]
+    card_number: str
+    order_amount: float
+    def __init__(self, card_number: _Optional[str] = ..., order_amount: _Optional[float] = ...) -> None: ...
 
-class HelloResponse(_message.Message):
-    __slots__ = ("greeting",)
-    GREETING_FIELD_NUMBER: _ClassVar[int]
-    greeting: str
-    def __init__(self, greeting: _Optional[str] = ...) -> None: ...
+class FraudResponse(_message.Message):
+    __slots__ = ("is_fraud",)
+    IS_FRAUD_FIELD_NUMBER: _ClassVar[int]
+    is_fraud: bool
+    def __init__(self, is_fraud: bool = ...) -> None: ...
